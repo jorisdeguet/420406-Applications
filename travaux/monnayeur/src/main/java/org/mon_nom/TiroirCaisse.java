992675427d6fd9bd8b65f060c41f64852f20bde7
Joris Deguet <joris.deguet@gmail.com>
2017-03-06 13:21:59 -0500

    Fix to avoid interfaces collision

--- travaux/monnayeur/src/main/java/org/mon_nom/TiroirCaisse.java
+++ travaux/monnayeur/src/main/java/org/mon_nom/TiroirCaisse.java
@@ -1,7 +1,33 @@
 package org.mon_nom;
 
-public interface TiroirCaisse extends Change{
+public interface TiroirCaisse {
 
+    /**
+     * Renvoie le nombre d'items de ce type contenu dans cet objet
+     * @param m type
+     * @return
+     */
+    int nombreItemsPour(ArgentObjet m);
+
+    /**
+     * Ajouter un objet du type spécifié m fois.
+     * @param m
+     * @param nombre
+     */
+    void ajouterItem(ArgentObjet m, int nombre);
+
+    /**
+     * Renvoie la valeur totale de l'objet en dollars
+     * @return
+     */
+    double valeurTotale();
+
+    /**
+     * Renvoie le nombre total d'items contenu dans cet objet.
+     * @return
+     */
+    int nombreTotalItems();
+    
     /**
      * Renvoie la capacité maximale de ce Tiroir pour ce type d'items
      * @param m
