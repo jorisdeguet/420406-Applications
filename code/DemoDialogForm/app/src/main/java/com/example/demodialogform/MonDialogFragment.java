b6388335e625c54c75cc7b669e3e38f1fd16268f
Joris Deguet <joris.deguet@gmail.com>
2018-01-05 13:34:39 -0500

    Fix on Dialogs

--- code/DemoDialogForm/app/src/main/java/com/example/demodialogform/MonDialogFragment.java
+++ code/DemoDialogForm/app/src/main/java/com/example/demodialogform/MonDialogFragment.java
@@ -6,37 +6,25 @@ import android.os.*;
 import android.view.*;
 import android.widget.*;
 
+import java.util.Arrays;
+import java.util.List;
+
+import static com.example.demodialogform.R.id.code;
+import static com.example.demodialogform.R.id.spinner;
+
 public class MonDialogFragment extends DialogFragment {
-/*
-	@Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-            Bundle savedInstanceState) {
-		// mise en place de l'interface a partir du XML
-        final View v = inflater.inflate(R.layout.mon_dialog, container, false);
-       
-        // recuperation de chaque element d'interface
-        final Button button = (Button)v.findViewById(R.id.add);
-        final EditText code = (EditText) v.findViewById(R.id.code);
-        final Spinner spinner = (Spinner) v.findViewById(R.id.spinner);
-        
-		// ajout des elements au spinner
-        List<String> listes = Arrays.asList("Homme","Femme");
-		ArrayAdapter<String> adapter = new ArrayAdapter<String>(v.getContext(), android.R.layout.simple_spinner_item, listes);
-		adapter.setDropDownViewResource(android.R.layout.simple_expandable_list_item_1);
-		spinner.setAdapter(adapter);
-		
-       
-        button.setOnClickListener(new OnClickListener() {
-            public void onClick(View v) {
-                
 
-                MonDialogFragment.this.dismiss();
-            }
-        });
-
-        return v;
-    }
-*/
+    /**
+     * Uses a standard dialog to handle the title and both positive and negative buttons
+     * with a builder.
+     *
+     * We specified a custom view for the main part of the dialog.
+     *
+     * We access the custom view to retrieve the information.
+     *
+     * @param savedInstanceState
+     * @return
+     */
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         final AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
@@ -48,18 +36,28 @@ public class MonDialogFragment extends DialogFragment {
         // Pass null as the parent view because its going in the dialog layout
         final View v = inflater.inflate(R.layout.mon_dialog, null);
         builder.setView(v);
+
+        final EditText code = (EditText) v.findViewById(R.id.code);
+        final Spinner spinner = (Spinner) v.findViewById(R.id.spinner);
+
+        // add elements to spinner
+        List<String> list = Arrays.asList("Homme","Femme");
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity(),
+                R.array.sexes, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
         // Add action buttons
         builder.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int id) {
-                        final EditText code = (EditText) v.findViewById(R.id.code);
-                        final Spinner spinner = (Spinner) v.findViewById(R.id.spinner);
-                        // TODO
-                        Toast.makeText(getActivity(), "Yo " + code.getText(), Toast.LENGTH_SHORT).show();
+                        // TODO Recover all values and perform the action
+                        Toast.makeText(getActivity(), "Yo " + spinner.getSelectedItemPosition(), Toast.LENGTH_SHORT).show();
                     }
                 })
                 .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
+                        Toast.makeText(getActivity(), "Cancelled ", Toast.LENGTH_SHORT).show();
                         MonDialogFragment.this.getDialog().cancel();
                     }
                 });
