f5078b4bac741eb1a3f151ebab24dbdde3361b01
Maude <whippou@gmail.com>
2020-05-07 11:42:53 -0400

    Added demo for graphs

---
+++ code/DemoGraphique/app/src/main/java/com/example/demographique/MainActivity.java
@@ -0,0 +1,97 @@
+package com.example.demographique;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.formatter.DefaultAxisValueFormatter;
+import com.github.mikephil.charting.interfaces.datasets.IBarDataSet;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+    BarChart chart;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setTitle("Mon beau graphique");
+
+        chart = findViewById(R.id.chart);
+
+
+        /* Settings for the graph - Change me if you want*/
+        chart.setMaxVisibleValueCount(6);
+        chart.setPinchZoom(false);
+        chart.setDrawGridBackground(false);
+
+        XAxis xAxis = chart.getXAxis();
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+        xAxis.setDrawGridLines(false);
+        xAxis.setGranularity(1); // only intervals of 1 day
+        xAxis.setLabelCount(7);
+        xAxis.setValueFormatter(new DefaultAxisValueFormatter(0));
+
+        YAxis leftAxis = chart.getAxisLeft();
+        leftAxis.setLabelCount(8, false);
+        leftAxis.setPosition(YAxis.YAxisLabelPosition.OUTSIDE_CHART);
+        leftAxis.setSpaceTop(15f);
+        leftAxis.setGranularity(1);
+        leftAxis.setAxisMinimum(0f);
+        leftAxis.setValueFormatter(new DefaultAxisValueFormatter(0));
+        chart.getDescription().setEnabled(false);
+        chart.getAxisRight().setEnabled(false);
+
+
+
+        /* Data and function call to bind the data to the graph */
+        Map<Integer, Integer> dataGraph = new HashMap<Integer, Integer>() {{
+            put(5, 10);
+            put(3, 3);
+            put(7, 2);
+            put(2, 1);
+            put(9, 0);
+        }};
+        setData(dataGraph);
+    }
+
+    private void setData(Map<Integer, Integer> datas) {
+
+        ArrayList<BarEntry> values = new ArrayList<>();
+
+        /* Every bar entry is a bar in the graphic */
+        for (Map.Entry<Integer, Integer> key : datas.entrySet()){
+            values.add(new BarEntry(key.getKey() , key.getValue()));
+        }
+
+        BarDataSet set1;
+        if (chart.getData() != null &&
+                chart.getData().getDataSetCount() > 0) {
+            set1 = (BarDataSet) chart.getData().getDataSetByIndex(0);
+            set1.setValues(values);
+            chart.getData().notifyDataChanged();
+            chart.notifyDataSetChanged();
+        } else {
+            set1 = new BarDataSet(values, "Points");
+
+            set1.setDrawIcons(false);
+
+            ArrayList<IBarDataSet> dataSets = new ArrayList<>();
+            dataSets.add(set1);
+
+            BarData data = new BarData(dataSets);
+            data.setValueTextSize(10f);
+            data.setBarWidth(.9f);
+            chart.setData(data);
+        }
+    }
+}
